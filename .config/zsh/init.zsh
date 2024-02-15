62bb46da1ee3209fcb55b59f3fc79089f991841f
jeremytondo <jmargari@gmail.com>
2024-02-15 22:59:48 +0000

    Reorganized file structure

---
+++ .config/zsh/init.zsh
@@ -0,0 +1,21 @@
+# +---------------+
+# | SHARED CONFIG |
+# +---------------+ 
+# Initialization creatd by running ./config/setup.sh.
+# Links the relevant configs to this .zshrc file.
+# Don't change the `Start Init -->` or `<-- End Init`
+# comments, they're used for future automated updates.
+# 
+# For more infomrtion see ~/.config/readme.md
+
+# Start Init -->
+
+source ~/.dotfiles/zsh/main.zsh
+
+# <-- End Init
+
+# +---------------+
+# | CUSTOM CONFIG |
+# +---------------+
+# Everything below is custom config for this device.
+
---
+++ .config/zsh/main.zsh
@@ -0,0 +1,62 @@
+# +-----------------+
+# | GLOBAL SETTINGS |
+# +-----------------+
+
+dotfiles="$HOME/.dotfiles"
+
+# +---------------+
+# | INITIAL SETUP |
+# +---------------+
+
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.histfile
+HISTSIZE=1000
+SAVEHIST=1000
+bindkey -e
+# End of lines configured by zsh-newuser-install
+# The following lines were added by compinstall
+zstyle :compinstall filename "/home/${USER}/.zshrc"
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
+
+# +------------------+
+# | Auto Completions |
+# +------------------+
+
+source $dotfiles/zsh-plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+
+# +---------+
+# | Aliases |
+# +---------+
+
+source $dotfiles/zsh/aliases.zsh
+
+# +--------+
+# | PROMPT |
+# +--------+
+
+# Prmopt Setup
+export STARSHIP_CONFIG="$dotfiles/starship/starship.toml"
+eval "$(starship init zsh)"
+
+# +--------+
+# | NEOVIM |
+# +--------+
+
+export PATH="$PATH:/opt/nvim-linux64/bin"
+
+# +------------------+
+# | Config Directory |
+# +------------------+
+
+export XDG_CONFIG_HOME="$HOME/.config"
+
+# +------------+
+# | NVM Config |
+# +------------+
+
+export NVM_DIR="$HOME/.config/nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
