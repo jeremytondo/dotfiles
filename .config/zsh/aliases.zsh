e05fffc0642380af1e4f46bc6a9fcfa8ca6e1fc2
jeremytondo <jmargari@gmail.com>
2024-02-23 13:19:45 -0600

    Fixing some stuff

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -9,8 +9,13 @@ alias tma="tmux a"
 alias tmat="tmux a -t"
 alias tml="tmux ls"
 alias tmks="tmux kill-server"
+
+# List running tmux servers
 alias tmsl="lsof -U | grep '^tmux'"
+
+# TourneyMate tmux server
 alias tmtm="tmux -L tourney-mate-api"
+alias tmtml="tmux -L tourney-mate-api ls"
 alias tmtmks="tmux -L tourney-mate-api kill-server"
 alias tmtma="tmux -L tourney-mate-api a -t tourney-mate-api"
 alias tmstm="tmux detach-client; tmtma"
